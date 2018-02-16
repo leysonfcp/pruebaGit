97de0d397eb95a4ef654ce2ce49b632175692a2d
leysonfcp <36524370+leysonfcp@users.noreply.github.com>
2018-02-15 20:19:30 -0500

    Add files via upload
    Agregando prueba

---
+++ pruebaGit/src/co/com/pruebaGit/pruebaGit.java
@@ -0,0 +1,16 @@
+package co.com.pruebaGit;
+
+import java.io.Console;
+
+public class pruebaGit {
+
+	public pruebaGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+	}
+
+}
